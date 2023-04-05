5d064ed2a6da48df0749b37ad6bcc7ec75a0e9b6
priya25270 <km894274@gmail.com>
2023-04-05 16:01:42 +0530

    First commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+
+import Destination from './Destination';
+import About from './About';
+import Contact from './Contact';
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+    <About/>
+    <Destination/>
+    <Contact/>
     </div>
   );
 }
---
+++ src/Destination.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Destination() {
+  return (
+    <div>
+      <h2>Destination</h2>
+      <p>Find your dream destination here!</p>
+    </div>
+  );
+}
+
+export default Destination;
